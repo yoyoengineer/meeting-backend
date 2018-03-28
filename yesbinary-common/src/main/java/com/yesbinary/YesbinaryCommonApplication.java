2aa1da72d39e4f07fb60179ec57b05519f331b80
yoyoengineer <yoyoengineer@outlook.com>
2018-03-28 14:33:38 +0800

    commit the yesbinary-common.

---
+++ yesbinary-common/src/main/java/com/yesbinary/YesbinaryCommonApplication.java
@@ -0,0 +1,12 @@
+package com.yesbinary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesbinaryCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesbinaryCommonApplication.class, args);
+	}
+}
